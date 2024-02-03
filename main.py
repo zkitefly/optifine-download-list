ca72c5db6e482ce1de4baaf081baa7bc9eb8d8fe
zkitefly <z18344203426@qq.com>
2024-02-03 14:23:09 +0800

    调整 of-302 链接的顺序

--- main.py
+++ main.py
@@ -68,12 +68,19 @@ def main():
 
     # 覆盖download字段
     existing_index["download"] = [
-        "https://of-302.burningtnt.workers.dev/file/",
-        "https://of-302.zkitefly.eu.org/file/",
-        "https://of-302v.zkitefly.eu.org/file/",
-        "https://of-302-cf.8mi.edu.pl/file/",
         "https://of-302-v.8mi.edu.pl/file/",
+        "https://of-302-cf.8mi.edu.pl/file/",
+        "https://of-302v.zkitefly.eu.org/file/",
+        "https://of-302.zkitefly.eu.org/file/",
+        "https://of-302.burningtnt.workers.dev/file/",
     ]
+    # existing_index["download"] = [
+    #     "https://of-302.burningtnt.workers.dev/file/",
+    #     "https://of-302.zkitefly.eu.org/file/",
+    #     "https://of-302v.zkitefly.eu.org/file/",
+    #     "https://of-302-cf.8mi.edu.pl/file/",
+    #     "https://of-302-v.8mi.edu.pl/file/",
+    # ]
 
     # 合并数据
     existing_index["file"].extend(versions)
